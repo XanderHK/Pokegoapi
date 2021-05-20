97eafb9a42eea0702301af1b187525bf8b85a378
XanderHK <xander.korts@gmail.com>
2021-05-20 19:52:48 +0200

    again

--- server/src/app/main.go
+++ server/src/app/main.go
@@ -42,7 +42,6 @@ func main() {
     json.Unmarshal(responseData, &responseObject)
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        
     fmt.Fprintf(w, responseObject.Name)
         for i := 0; i < len(responseObject.Pokemon); i++ {
             fmt.Fprintf(w, responseObject.Pokemon[i].Species.Name)
