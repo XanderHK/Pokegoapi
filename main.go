488b4660016e726dac8655903d6129d9c5965085
XanderHK <xander.korts@gmail.com>
2021-07-01 13:41:16 +0200

    refactoring, import and structure adjustments

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"github.com/XanderHK/Pokegoapi/server"
-	"github.com/XanderHK/Pokegoapi/server/src/commands"
 )
 
 func main() {
 	server.Start()
-	go commands.Scanner()
+	// go commands.Scanner()
 }
